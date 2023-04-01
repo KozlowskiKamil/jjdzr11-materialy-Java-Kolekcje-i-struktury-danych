340c80abb98aacd5e6a6f17234e36f03ae7beed8
tomlisow <tomasz.lisowski@proton.me>
2023-04-01 15:38:58 +0200

    Task 1a

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -3,6 +3,20 @@ package com.infoshareacademy;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+        Integer[] duplicatedValues = copyTable(1, 2, 3, 4, 5);
+        for (int i = 0; i < duplicatedValues.length; i++) {
+            System.out.println(duplicatedValues[i]);
+        }
+    }
+
+    private static Integer[] copyTable(Integer... params) {
+        Integer[] result = new Integer[params.length];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = params[i] * 2;
+        }
+
+        return result;
     }
 }
