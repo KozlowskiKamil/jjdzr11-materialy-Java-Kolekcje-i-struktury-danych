7f39a7daa3035ad3a694e93071d41b20619eaa53
tomlisow <tomasz.lisowski@proton.me>
2023-04-01 17:49:11 +0200

    Task 9

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -2,6 +2,7 @@ package com.infoshareacademy;
 
 
 import com.infoshareacademy.factories.CarFactory;
+import com.infoshareacademy.factories.EngineFactory;
 import com.infoshareacademy.model.Car;
 import com.infoshareacademy.model.Engine;
 
@@ -9,13 +10,26 @@ import java.util.*;
 
 public class App {
     public static void main( String[] args ) {
+        Engine engine1 = EngineFactory.generateEngine();
+        Engine engine2 = EngineFactory.generateEngine();
+        Engine engine3 = new Engine(100, 1000);
+        Engine engine4 = new Engine(100, 1000);
 
-        Set<String> strings = new HashSet<>();
-        strings.add("info");
-        strings.add("Share");
-        strings.add("Academy");
+        Set<Engine> engines = copyTable(engine1, engine2, engine3, engine4);
+        System.out.println(engines.size());
+        for (Engine engine : engines) {
+            System.out.println(engine);
+        }
+    }
 
-        System.out.println(strings.contains("info"));
-        System.out.println(strings.contains("INFO"));
+    private static Set<Engine> copyTable(Engine... engines) {
+        //return new HashSet<>(Arrays.asList(engines));
+
+        Set<Engine> result = new HashSet<>();
+
+        for (Engine engine : engines) {
+            result.add(engine);
+        }
+        return result;
     }
 }
