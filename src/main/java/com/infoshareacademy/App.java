c41e984c144d73450a1a8c9ac37006df411334e7
tomlisow <tomasz.lisowski@proton.me>
2023-04-01 17:38:05 +0200

    Task 8

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -3,33 +3,19 @@ package com.infoshareacademy;
 
 import com.infoshareacademy.factories.CarFactory;
 import com.infoshareacademy.model.Car;
+import com.infoshareacademy.model.Engine;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class App {
     public static void main( String[] args ) {
 
-        Car car1 = CarFactory.createRandomCar();
-        Car car2 = CarFactory.createRandomCar();
-        Car car3 = CarFactory.createRandomCar();
+        Set<String> strings = new HashSet<>();
+        strings.add("info");
+        strings.add("Share");
+        strings.add("Academy");
 
-        List<Car> cars = copyTable(car1, car2, car3);
-
-        for (Car car : cars) {
-            System.out.println(car);
-        }
-    }
-
-    private static List<Car> copyTable(Car... cars) {
-//        return new ArrayList<>(Arrays.asList(cars));
-
-        List<Car> result = new ArrayList<>();
-        for (Car car : cars) {
-            result.add(car);
-        }
-
-        return result;
+        System.out.println(strings.contains("info"));
+        System.out.println(strings.contains("INFO"));
     }
 }
