b3409a6713e4c315159335aa6aab5727f2f2fe8f
tomlisow <tomasz.lisowski@proton.me>
2023-04-15 20:46:15 +0200

    iterator example

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -4,40 +4,24 @@ package com.infoshareacademy;
 import com.infoshareacademy.factories.CarFactory;
 import com.infoshareacademy.model.Car;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class App {
     public static void main( String[] args ) {
-        List<Car> cars = CarFactory.createRandomCars(5);
-        cars.forEach(System.out::println);
+//        List<Car> cars = CarFactory.createRandomCars(5);
+//        cars.forEach(System.out::println);
 
-        Map<Integer, List<Car>> groupedCars = groupCars(cars);
+        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5));
 
-        System.out.println(groupedCars);
-    }
+        Iterator<Integer> iterator = numbers.iterator();
 
-    private static Map<Integer, List<Car>> groupCars(List<Car> cars) {
-        Map<Integer, List<Car>> groupedCars = new HashMap<>();
-
-        for (Car car : cars) {
-            Integer key = car.getEngine().getCapacity();
-
-            //dodaję do mapy pustą listę, jeżeli pod danym kluczem nic jeszcze nie ma
-            groupedCars.putIfAbsent(key, new ArrayList<>());
-
-            //tempValue - lista wartości obecnie znajdujących się pod danym kluczem
-            List<Car> tempValue = groupedCars.get(key);
-
-            //dodaję element do powyższej listy
-            tempValue.add(car);
-
-            //nadpisuję mapę, pod dany klucz wrzucam nową listę (poprzednia + 1 element)
-            groupedCars.put(key, tempValue);
+        System.out.println(numbers);
+        while (iterator.hasNext()) {
+            Integer nextValue = iterator.next();
+            if (nextValue % 2 == 1) {
+                iterator.remove();
+            }
         }
-
-        return groupedCars;
+        System.out.println(numbers);
     }
 }
