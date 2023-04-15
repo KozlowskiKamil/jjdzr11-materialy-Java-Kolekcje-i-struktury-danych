6383d3941a7dec4aa8b85c2d8139c1a7e535b318
tomlisow <tomasz.lisowski@proton.me>
2023-04-15 19:20:15 +0200

    Task 10b

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -8,34 +8,18 @@ import java.util.Set;
 public class App {
     public static void main( String[] args ) {
 
-        Integer k1 = 1;
-        Integer k2 = 2;
-        Integer k3 = 3;
-        Integer k4 = 2;
-
-        String v1 = "v1";
-        String v2 = "v2";
-        String v3 = "v2";
-        String v4 = "v4";
-
         Map<Integer, String> map = new HashMap<>();
-        map.putIfAbsent(k1, v1);
-        map.putIfAbsent(k2, v2);
-        map.putIfAbsent(k3, v3);
-        map.putIfAbsent(k4, v4);
+        map.put(1, "ISA");
+        map.put(2, "info");
+        map.put(3, "Share");
+        map.put(5, "Java");
+//        map.put(4, null);
 
         System.out.println(map);
-        System.out.println(map.keySet());
-        System.out.println(map.values());
+        System.out.println(map.get(4));
+        System.out.println(map.get(5));
+        System.out.println(map.containsKey(15));
+        System.out.println(map.containsValue("ISA"));
 
-        for (Integer key : map.keySet()) {
-            String value = map.get(key);
-            System.out.println(key + " - " + value);
-        }
-
-        Set<Map.Entry<Integer, String>> entries = map.entrySet();
-        for (Map.Entry entry : entries) {
-            System.out.println(entry);
-        }
     }
 }
