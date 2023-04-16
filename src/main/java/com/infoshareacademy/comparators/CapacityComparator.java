86528dd0b8b255a36826d27a526f0bc097eaabc5
tomlisow <tomasz.lisowski@proton.me>
2023-04-16 16:06:53 +0200

    Task 15b - fixed

---
+++ src/main/java/com/infoshareacademy/comparators/CapacityComparator.java
@@ -0,0 +1,12 @@
+package com.infoshareacademy.comparators;
+
+import com.infoshareacademy.model.Car;
+
+import java.util.Comparator;
+
+public class CapacityComparator implements Comparator<Car> {
+    @Override
+    public int compare(Car o1, Car o2) {
+        return o1.getEngine().getCapacity().compareTo(o2.getEngine().getCapacity());
+    }
+}
