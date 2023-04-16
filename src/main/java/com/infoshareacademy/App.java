009a1c909662f42d79e5f964c90f3002b9651481
tomlisow <tomasz.lisowski@proton.me>
2023-04-16 17:04:52 +0200

    Task 18a

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,26 +1,30 @@
 package com.infoshareacademy;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.infoshareacademy.factories.EngineFactory;
 import com.infoshareacademy.model.Engine;
 
 import java.io.IOException;
+import java.util.List;
 
 public class App {
     public static void main( String[] args ) throws IOException {
+
         Engine engine = EngineFactory.generateEngine();
         System.out.println(engine);
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setPrettyPrinting();
+        Gson gson = gsonBuilder.create();
 
-        String json = objectMapper
-                .writerWithDefaultPrettyPrinter()
-                .writeValueAsString(engine);
+        String json = gson.toJson(engine);
         System.out.println(json);
 
-        Engine fromJson = objectMapper.readValue(json, Engine.class);
+        Engine fromJson = gson.fromJson(json, Engine.class);
         System.out.println(fromJson);
-        System.out.println(engine.equals(fromJson));
     }
 }
