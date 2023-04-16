1240a53f247c61b222974fb4f6720fce068daa32
tomlisow <tomasz.lisowski@proton.me>
2023-04-16 17:09:44 +0200

    Task 18b

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -14,17 +14,20 @@ import java.util.List;
 public class App {
     public static void main( String[] args ) throws IOException {
 
-        Engine engine = EngineFactory.generateEngine();
-        System.out.println(engine);
+        Engine engine1 = EngineFactory.generateEngine();
+        Engine engine2 = EngineFactory.generateEngine();
+        Engine[] engines = {engine1, engine2};
 
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.setPrettyPrinting();
         Gson gson = gsonBuilder.create();
 
-        String json = gson.toJson(engine);
+        String json = gson.toJson(engines);
         System.out.println(json);
 
-        Engine fromJson = gson.fromJson(json, Engine.class);
-        System.out.println(fromJson);
+        Engine[] fromJson = gson.fromJson(json, Engine[].class);
+        for (Engine engine : fromJson) {
+            System.out.println(engine);
+        }
     }
 }
